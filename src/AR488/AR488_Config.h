f3a105b05cb30157b1b2b912cdd7050301cffe30
JohnCh <subs@qcontinuum.plus.com>
2020-01-24 14:10:43 +0000

    0.48.07

--- src/AR488/AR488_Config.h
+++ src/AR488/AR488_Config.h
@@ -68,9 +68,9 @@
 /*** MEGA 2560 board ***/
 #elif __AVR_ATmega2560__
   /*** Board/layout selection ***/
-  //#define AR488_MEGA2560_D
+  #define AR488_MEGA2560_D
   //#define AR488_MEGA2560_E1
-  #define AR488_MEGA2560_E2
+  //#define AR488_MEGA2560_E2
   /*** Serial ports ***/
   // Mega 2560 supports Serial, Serial1, Serial2, Serial3. Since the pins 
   // associated with Serial2 are used in the default pin layout, Serial2
@@ -158,7 +158,7 @@
  * This will require the use of an additional GPIO pin to control
  * the read and write modes of the ICs.
  */
-#define SN7516X
+//#define SN7516X
 #ifdef SN7516X
   #define SN7516X_TE 6
 //  #define SN7516X_DC 13
